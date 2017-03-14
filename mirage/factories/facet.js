387c3a04b070796140ae8e8015db53060a39816a
Aaron Fischer <aaron.fischer@caxiam.com>
2017-03-14 15:19:13 -0400

    Add back in the slow afterCreate()

--- mirage/factories/facet.js
+++ mirage/factories/facet.js
@@ -8,15 +8,15 @@ export default Mirage.Factory.extend({
     return faker.name.firstName();
   },
 
-  // afterCreate(facet, server) {
-  //   // force mirage to save inverse root relationship, set the root.children hasMany
-  //   // PAINFULLY SLOW
-  //   // TODO investigate once mirage has stable 0.3.0 build
-  //   let root = server.schema.facets.where({ id: facet.rootId }).models[0];
-  //   // attempt only if root exists
-  //   if (root) {
-  //     root.childIds = root.childIds.concat(facet.id);
-  //     root.save();
-  //   }
-  // },
+  afterCreate(facet, server) {
+    // force mirage to save inverse root relationship, set the root.children hasMany
+    // PAINFULLY SLOW
+    // TODO investigate once mirage has stable 0.3.0 build
+    let root = server.schema.facets.where({ id: facet.rootId }).models[0];
+    // attempt only if root exists
+    if (root) {
+      root.childIds = root.childIds.concat(facet.id);
+      root.save();
+    }
+  },
 });
